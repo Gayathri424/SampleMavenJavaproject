dae6c10ce0b6d5ddf685d40619c92c1ad1cc272e
Gayathri Velishala <velishalagayathri2005@gmail.com>
2025-12-02 10:03:32 +0530

    Update greeting message in App.java

--- src/main/java/com/app/demo/samplemavenjava/App.java
+++ src/main/java/com/app/demo/samplemavenjava/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Hii");
+        System.out.println("Hii how are you");
     }
 }
